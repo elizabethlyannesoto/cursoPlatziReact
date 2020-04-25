3c9483dd79c9a31f5aacd4052a19ed97f52a0809
elizabethlyannesoto <37382167+elizabethlyannesoto@users.noreply.github.com>
2020-04-25 01:01:03 -0500

    Avance de proyecto

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -3,11 +3,20 @@ import React from 'react';
 class BadgesList extends React.Component{
     render(){
         return (
-            <ul className="list-unstyled">
+            <ul className="list-unstyled BadgesList">
                 {this.props.batges.map((badge) => {
                         return(
                             <li key={badge.id}>
-                                <p> {badge.firstName}{badge.lastName}</p>
+                                <div className="container">
+                                    <div className="row BadgesListItem">
+                                        <div className="col-2 contenedorList">
+                                            <img className="BadgesListItem__avatar" src="https://s.gravatar.com/avatar/9b44ce3fd90b33ce0633d780e8d3dde9?s=80" alt="Avatar" />
+                                        </div>
+                                        <div className="col-10">
+                                            <p> {badge.firstName}{badge.lastName}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </li>
                         )
                     }
